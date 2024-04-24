ce0b76af3f2e533f654dab7c40e9e4ecc305c4a5
MattewGG <mateusgasda@gmail.com>
2024-04-24 20:40:31 -0300

    Categorias e Produtos Service

---
+++ src/service/categoria.js
@@ -0,0 +1,52 @@
+const mysql = require("mysql2/promise");
+const databaseConfig = require("../config/database.js");
+const { emit } = require("nodemon");
+const { updateCategoria } = require("./categoria.js");
+const { use } = require("../router/categoriaRouter.js");
+const { Connection } = require("mysql2/typings/mysql/lib/Connection");
+
+async function getAllcategoria() {
+  const connnetion = await mysql.createConnection(databaseConfig);
+  const [rows] = await Connection.query("SELECT * FROM product");
+
+  await connnetion.end();
+  return rows;
+}
+
+async function createCategoria(nome, descricao) {
+  const connnetion = await mysql.createCategoria(databaseConfig);
+  const insertCategoria = "INSERT into product(nome, descricao) VALUES(?, ?)";
+  await connnetion.query(insertProduct, [nome, descricao]);
+  await connnetion.end();
+}
+
+async function updateCategoria(nome, descricao) {
+  const connection = await mysql.createCategoria(databaseConfig);
+  await connection.query(updateProduct, [nome, descricao]);
+  await connection.end();
+}
+
+async function deleteCategoria(id) {
+  const connection = await mysql.createConnection(databaseConfig);
+  await connection.query("DELETE FROM categoria WHERE id = ?", [id]);
+  await connection.end();
+}
+
+async function getAllcategoriaById(id) {
+  const connection = await mysql.createConnection(databaseConfig);
+  const [categoria] = await connection.query(
+    "SELECT * FROM product WHERE id = ?",
+    [id]
+  );
+
+  await connection.end();
+  return product;
+}
+
+module.exports = {
+  getAllcategoria,
+  createCategoria,
+  updateCategoria,
+  deleteCategoria,
+  getAllcategoriaById,
+};
---
+++ src/service/produto.js
@@ -0,0 +1,81 @@
+const mysql = require("mysql2/promise");
+const databaseConfig = require("../config/database.js");
+const { emit } = require("nodemon");
+const { updateProduct } = require("./product.js");
+const { use } = require("../router/productRouter.js");
+const { Connection } = require("mysql2/typings/mysql/lib/Connection");
+
+async function getAllproduct() {
+  const connnetion = await mysql.createConnection(databaseConfig);
+  const [rows] = await Connection.query("SELECT * FROM product");
+
+  await connnetion.end();
+  return rows;
+}
+
+async function createProduct(
+  nome,
+  preco,
+  descricao,
+  quantidade,
+  idCategoria,
+  idUsuario
+) {
+  const connnetion = await mysql.createConnection(databaseConfig);
+  const insertProduct =
+    "INSERT into product(nome, preco, descricao, quantidade, idCategoria, idUsuario) VALUES(?, ?, ?, ?, ?, ?)";
+  await connnetion.query(insertProduct, [
+    nome,
+    preco,
+    descricao,
+    quantidade,
+    idCategoria,
+    idUsuario,
+  ]);
+  await connnetion.end();
+}
+
+async function updateProduct(
+  nome,
+  preco,
+  descricao,
+  quantidade,
+  idCategoria,
+  idUsuario
+) {
+  const connection = await mysql.createConnection(databaseConfig);
+  await connection.query(updateProduct, [
+    nome,
+    preco,
+    descricao,
+    quantidade,
+    idCategoria,
+    idUsuario,
+  ]);
+  await connection.end();
+}
+
+async function deleteProduct(id) {
+  const connection = await mysql.createConnection(databaseConfig);
+  await connection.query("DELETE FROM product WHERE id = ?", [id]);
+  await connection.end();
+}
+
+async function getAllproductById(id) {
+  const connection = await mysql.createConnection(databaseConfig);
+  const [product] = await connection.query(
+    "SELECT * FROM product WHERE id = ?",
+    [id]
+  );
+
+  await connection.end();
+  return product;
+}
+
+module.exports = {
+  getAllproduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getAllproductById,
+};
