b8a7fa98a309d782317d262aecbefcbc0cb70371
campestrinia <andrey.campes@gmail.com>
2024-04-24 19:53:03 -0300

    arrumando geral

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
-const cartasRouter = require("./router/cartasRouter.js");
+
+
 
 const PORT = 3001;
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors());
 
-app.use("/api", cartasRouter);
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello Word!</h1>`);
+});
 app.listen(PORT, () => {
   console.log("servidor online");
 });
