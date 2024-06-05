ead6a1acd1e6d6c797d46d4030dedaf214883631
campestrinia <andrey.campes@gmail.com>
2024-06-05 19:29:22 -0300

    corrigindo

---
+++ src/router/enderecoRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const enderecoRouter = require("../controller/enderecoController.js");
+
+router.get("/categoria",);
+router.post(`/usuario`,);
+router.put("/usuario/:id",);
+router.delete("/usuario/:id",);
+router.get("/usuario/:id",);
+
+module.exports = router;
