3c88af2541786b77e24316763111f917dd000545
MattewGG <mateusgasda@gmail.com>
2024-06-05 21:35:24 -0300

    porta adicionada

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const usuarioRouter = require("./router/usuarioRouter.js")
-const produtoRouter = require('./router/produtoRouter.js')
-const enderecoRouter = require('./router/enderecoRouter.js')
-const categoriaRouter = require('./router/categoriaRouter.js')
-
+const usuarioRouter = require("./router/usuarioRouter.js");
+const produtoRouter = require("./router/produtoRouter.js");
+const enderecoRouter = require("./router/enderecoRouter.js");
+const categoriaRouter = require("./router/categoriaRouter.js");
 
 const PORT = 3001;
 
@@ -12,18 +11,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
-
 app.get("/", (req, res) => {
   res.send(`<h1>Hello Word!</h1>`);
 });
 
 app.use(bodyParser.json());
-app.use("/usuario", usuarioRouter);
+app.use("/usuaario", usuarioRouter);
 app.use("/produto", produtoRouter);
 app.use("/endereco", enderecoRouter);
 app.use("/categoria", categoriaRouter);
 
-
 app.listen(PORT, () => {
   console.log("servidor online");
+  console.log(`http://localhost:${PORT}`);
 });
