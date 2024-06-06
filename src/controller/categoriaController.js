ede89b7241b0c614142d2ffab4f252dcb280dfb0
MattewGG <mateusgasda@gmail.com>
2024-06-06 20:13:07 -0300

    categoria

--- src/controller/categoriaController.js
+++ src/controller/categoriaController.js
@@ -1,81 +1,81 @@
-const { categoria } = require("../config/database.js");
-const categoriaService = require("../service/usuario.js");
+//const { categoria } = require("../config/database.js");
+const categoriaService = require("../service/categoria.js");
 
 async function getAllCategoria(req, res) {
-    try {
-        const rows = await categoriaService.getAllCategoria();
+  try {
+    const rows = await categoriaService.getAllCategoria();
 
-        res.status(200).json(rows);
-    } catch (error) {
-        res.status(500).send({
-            message: "Error getting users",
-            body: error.message,
-        });
-    }
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).send({
+      message: "Error getting categoria",
+      body: error.message,
+    });
+  }
 }
 
 async function createCategoria(req, res) {
-    const { nome, email, CPF, telefone, senha } = req.body;
+  const { nome, descricao } = req.body;
 
-    try {
-        await categoriaService.createCategoria(nome, email, CPF, telefone, senha);
+  try {
+    await categoriaService.createCategoria(nome, descricao);
 
-        res.status(201).json({ message: `Success` });
-    } catch (eror) {
-        res.status(500).send({
-            message: `error adding user!`,
-            error: error.message,
-        });
-    }
+    res.status(201).json({ message: `Success` });
+  } catch (eror) {
+    res.status(500).send({
+      message: `error adding categoria!`,
+      error: error.message,
+    });
+  }
 }
 async function updateCategoria(req, res) {
-    try {
-        const { id } = req.params;
-        const { nome, email, CPF, telefone, senha } = req.params;
-        await categoriaService.updateCategoria(id, nome, email, CPF, telefone, senha);
+  try {
+    const { id } = req.params;
+    const { nome, descricao } = req.params;
+    await categoriaService.updateCategoria(id, nome, descricao);
 
-        res.status(204).json("Success");
-    } catch (error) {
-        res.status(500).send({
-            message: `Error updating user`,
-            body: error.message,
-        });
-    }
+    res.status(204).json("Success");
+  } catch (error) {
+    res.status(500).send({
+      message: `Error updating categoria`,
+      body: error.message,
+    });
+  }
 }
 
 async function deleteCategoria(req, res) {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        await categoriaService.deleteCategoria(id);
-        res.status(200).send({ message: "Deleted user" });
-    } catch (error) {
-        res.status(500).send({
-            message: "Erros deleting user",
-            error: error.message,
-        });
-    }
+    await categoriaService.deleteCategoria(id);
+    res.status(200).send({ message: "Deleted categoria" });
+  } catch (error) {
+    res.status(500).send({
+      message: "Erros deleting categoria",
+      error: error.message,
+    });
+  }
 }
 
 async function getCategoriaById(req, res) {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const user = await categoriaService.getCategoriaById(id);
+    const categoria = await categoriaService.getCategoriaById(id);
 
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).send({
-            message: "Error getting user by ID",
-            error: error.message,
-        });
-    }
+    res.status(200).json(categoria);
+  } catch (error) {
+    res.status(500).send({
+      message: "Error getting categoria by ID",
+      error: error.message,
+    });
+  }
 }
 
 module.exports = {
-    getAllCategoria,
-    createCategoria,
-    updateCategoria,
-    deleteCategoria,
-    getCategoriaById
-}
+  getAllCategoria,
+  createCategoria,
+  updateCategoria,
+  deleteCategoria,
+  getCategoriaById,
+};
--- src/service/categoria.js
+++ src/service/categoria.js
@@ -1,27 +1,23 @@
 const mysql = require("mysql2/promise");
 const databaseConfig = require("../config/database.js");
-const { emit } = require("nodemon");
-const { updateCategoria } = require("./categoria.js");
-const { use } = require("../router/categoriaRouter.js");
-const { Connection } = require("mysql2/typings/mysql/lib/Connection");
 
-async function getAllcategoria() {
-  const connnetion = await mysql.createConnection(databaseConfig);
-  const [rows] = await Connection.query("SELECT * FROM categoria");
+async function getAllCategoria() {
+  const connection = await mysql.createConnection(databaseConfig);
+  const [rows] = await connection.query("SELECT * FROM categoria");
 
-  await connnetion.end();
+  await connection.end();
   return rows;
 }
 
 async function createCategoria(nome, descricao) {
-  const connnetion = await mysql.createCategoria(databaseConfig);
+  const connection = await mysql.createCategoria(databaseConfig);
   const insertCategoria = "INSERT into categoria(nome, descricao) VALUES(?, ?)";
-  await connnetion.query(insertCategoria, [nome, descricao]);
-  await connnetion.end();
+  await connection.query(insertCategoria, [nome, descricao]);
+  await connection.end();
 }
 
 async function updateCategoria(nome, descricao) {
-  const connection = await mysql.createCategoria(databaseConfig);
+  const connection = await mysql.createConnection(databaseConfig);
   const updateCategoria =
     "UPDATE categoria Set nome = ?,  descricao = ? WHERE id = ?";
   await connection.query(updateCategoria, [nome, descricao]);
@@ -47,7 +43,7 @@ async function getAllcategoriaById(id) {
 }
 
 module.exports = {
-  getAllcategoria,
+  getAllCategoria,
   createCategoria,
   updateCategoria,
   deleteCategoria,
