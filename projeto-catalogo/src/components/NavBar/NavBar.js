739e71b5dfb406bc25b6d4988431e4fb5e307c44
campestrinia <andrey.campes@gmail.com>
2024-06-13 20:27:54 -0300

    styled

--- projeto-catalogo/src/components/NavBar/NavBar.js
+++ projeto-catalogo/src/components/NavBar/NavBar.js
@@ -1,15 +1,20 @@
-
+import { Container, Logo, Menu } from "./navBar.css"
 export function NavBar() {
     return (<>
-        <nav>
-            <div>
+        <Container>
+            <Logo>
+                <h1>T</h1>
+            </Logo>
+            <Menu>
                 <ul>
                     <li><a href="/">Home</a></li>
                     <li><a href="/about">About</a></li>
                     <li><a href="/contact">Contact</a></li>
                 </ul>
-            </div>
-        </nav>
+            </Menu>
+
+
+        </Container>
     </>
     );
 }
---
+++ projeto-catalogo/src/components/NavBar/navBar.css.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  background-color: red;
+  justify-content: space-between;
+  `;
+const Logo = styled.div`
+  display: flex;
+  background-color: blue;
+    width: 100px;
+  `;
+const Menu = styled.div`
+  display: flex;
+  background-color: green;
+  width: 225px;
+  `;
+
+export { Container, Logo, Menu };
