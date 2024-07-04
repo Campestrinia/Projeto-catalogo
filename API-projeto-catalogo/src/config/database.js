4d46d616a2b57156581cee08379a59d155e9f928
Andrey Campestrini <140768619+Campestrinia@users.noreply.github.com>
2024-07-04 00:47:42 -0300

    Update database.js

--- API-projeto-catalogo/src/config/database.js
+++ API-projeto-catalogo/src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-  host: "localhost",
+  host: "projeto-catalogo.c1yweeqgaqj8.us-east-2.rds.amazonaws.com",
   user: "root",
-  password: "root",
+  password: "SenaiADS2024",
   database: "api_projeto",
 };
